3d8cde40628c4669d24096a89b5e6b6ade7f7cf6
ddonix <18959661712@189.cn>
2018-03-30 21:11:10 +0800

    init

---
+++ main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+from aip import AipSpeech
+
+""" 你的 APPID AK SK """
+APP_ID = '11025174'
+API_KEY = 'AVn9EvSNIMvLzF7Uaf309nkM'
+SECRET_KEY = 'GVbbscDlDlGREoWTib5Om1q6WPFlEGpm'
+
+# 读取文件
+def get_file_content(filePath):
+	with open(filePath, 'rb') as fp:
+		return fp.read()
+
+# 识别本地文件
+client = AipSpeech(APP_ID, API_KEY, SECRET_KEY)
+res = client.asr(get_file_content('output.wav'), 'wav', 16000, {'dev_pid': '1536',})
+print res.get(u'result')[0]
