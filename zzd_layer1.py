bd60e1d4b53ec2d3743c67c4628c3e7b32c253ca
ddonix <18959661712@189.cn>
2018-04-02 16:16:38 +0800

    define|2018-04-02_16:16:38

--- zzd_layer1.py
+++ zzd_layer1.py
@@ -1,4 +1,14 @@
 # coding: utf-8
+
+#init:初始化。
+#stand：完成一次交互，等待下一次交互。没有预期。
+#wait:期待human的回应。有预期。
+#initiative:主动状态，主动发起对话。
+STATE = ('init', 'stand', 'wait')
+
+#0:工作模式；1:娱乐模式;2:训练模式;3:调试模式.
+MODE = ('work', 'train', 'debug')
+
 class zzdLayer1:
 	inSentenceClass = []		#输入语句类型
 	defineDict = {}
@@ -6,6 +16,8 @@ class zzdLayer1:
 	def __init__(self, corelayer0):
 		self.sentence = []
 		self.corelayer0 = corelayer0
+		self.state = 'init'		
+		self.mode = 'work'
 		self.init()
 	
 	def _solvesen(self, sen):
@@ -31,8 +43,11 @@ class zzdLayer1:
 		return 'solv:'+val
 	
 	def _sorrysen(self, sen):
-		outs = u'sorr:对不起，我无法处理\"'+sen+'\"'
-		return outs
+		if sen[0:5] == u'copy:':
+			return sen
+		else:
+			outs = u'sorr:对不起，我无法处理\"'+sen+'\"'
+			return outs
 	
 	def _copysen(self, sen):
 		outs = sen
@@ -42,15 +57,6 @@ class zzdLayer1:
 		outs = u'debu:调试模式'
 		return outs
 	
-	def _definesen_init(self):
-		zzdLayer1.defineDict[u'爱'] = u'在一起'
-	
-	def _definesen(self, sen):
-		o = zzdLayer1.defineDict.get(sen[5:len(sen)])
-		if o == None:
-			return self._sorrysen(sen)
-		return sen+u'是'+o
-        
 	def inputs(self, sen):
 		outs = None
 		for t in zzdLayer1.inSentenceClass:
@@ -70,3 +76,14 @@ class zzdLayer1:
 		zzdLayer1.inSentenceClass.append([u'sorr:', zzdLayer1._sorrysen])			#sorry
 		zzdLayer1.inSentenceClass.append([u'solv:', zzdLayer1._solvesen])			#solve
 		zzdLayer1.inSentenceClass.append([u'defi:', zzdLayer1._definesen])			#define
+	
+	def _definesen_init(self):
+		zzdLayer1.defineDict[u'爱'] = u'在一起'
+	
+	def _definesen(self, sen):
+		define = sen[5:len(sen)]
+		o = zzdLayer1.defineDict.get(define)
+		if o == None:
+			return self._sorrysen(u'copy:对不起，我没有\"'+define+u'\"的定义。'+u'请进入训练模式，添加定义。')
+		return sen+u'是'+o
+        
