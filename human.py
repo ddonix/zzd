713067e9c152f2a0397c40967219cc1c43a89ba3
donix <18959661712@189.cn>
2018-04-02 12:45:22 +0800

    delete volid files

--- human.py
+++ human.py
@@ -6,13 +6,14 @@ class human(unity.unity):
 	def __init__(self, core):
 		unity.unity.__init__(self)
 		self.core = core
+		self.waalist = []
     
 	def act(self, dest, waa_out):
 		dest.echo(self, waa_out)
 		return None
 	
 	def echo(self, sour, waa_in):
-		return waa_in
+		raise NotImplementedError
     
 	def forword(self, dest, waa_out):
 		return waa_out
--- zzd_layer1.py
+++ zzd_layer1.py
@@ -1,5 +1,4 @@
 # coding: utf-8
-import zzd_expression 
 import voice 
 
 class zzdLayer1:
