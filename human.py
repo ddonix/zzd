3a19c5220f556c5261d26ed3c9c09800fd181d57
ddonix <18959661712@189.cn>
2018-04-02 10:01:04 +0800

    unity

---
+++ human.py
@@ -0,0 +1,15 @@
+# -*- coding:utf-8 -*- 
+import unity 
+
+class human(unity):
+    def __init__(self):
+        self.kernel = None
+        
+    def act(self, dest, waa_out):
+		return None
+    
+	def echo(self, sour, waa_in):
+		return None 
+    
+	def forword(self, dest, waa_out):
+		return None
